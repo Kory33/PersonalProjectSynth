0117727480c91c53a6f56dad5ad0ec24773fa867
Kory33 <konryoitdv@gmail.com>
2016-09-19 05:16:17 +0530

    added Parameter's implementation

---
+++ Parameters.cpp
@@ -0,0 +1,9 @@
+#include "Parameters.h"
+
+Steinberg::tresult Steinberg::Vst::PPSynth::ParameterContainer::setState(IBStream* stream) {
+	return kResultTrue;
+}
+
+Steinberg::tresult Steinberg::Vst::PPSynth::ParameterContainer::getState(IBStream* stream) {
+	return kResultTrue;
+}
